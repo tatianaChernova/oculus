364ff17016fe23e6b4a9d458a9c9646da854c1c3
Tatiana Chernova <fedoseeva.tv@gmail.com>
2023-09-18 12:32:25 +0300

    Finished adaptive design

--- js/main.js
+++ js/main.js
@@ -18,3 +18,8 @@ function open(evt) {
     tabTarget.classList.add('tabs__btn-item--active');
     document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
 }
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu--active');
+})
